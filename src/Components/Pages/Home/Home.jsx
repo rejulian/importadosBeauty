32f1bb7ea947000d9589e3b5e2d77b8b09b6cfe8
Re Julian <julianre2021@gmail.com>
2023-09-28 16:21:59 -0300

    Adding main section

--- src/Components/Pages/Home/Home.jsx
+++ src/Components/Pages/Home/Home.jsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './Home.css'
+import { db } from '../../../firebaseConfig';
+import { getDocs, collection, query, where } from 'firebase/firestore';
+import { Navigation, Pagination } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/swiper-bundle.css';
+import ItemList from '../ItemList/ItemList';
+
 
 const Home = () => {
+
+  const [products, setProducts] = useState([])
+  const refCollection = collection(db, "products");
+
+  const getProducts = async () => {
+    try {
+      let productsQuery = query(refCollection, where("promote", "==", true))
+      const response = await getDocs(productsQuery);
+      const productsArr = response.docs.map(product => {
+        return { ...product.data(), id: product.id };
+      });
+      setProducts(productsArr);
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  useEffect(() => {
+    getProducts()
+  }, [])
+
   return (
     <main>
       <section id='hero'>
@@ -9,6 +37,37 @@ const Home = () => {
         <h1>ImportadosBeauty SN</h1>
         <h2>productos del exterior a tu alcance</h2>
       </section>
+      <main id='main'>
+        <h1>Lo mas destacado</h1>
+        <Swiper
+          className='mySwiper'
+          slidesPerView={4}
+          spaceBetween={20}
+          pagination={{ clickable: true }}
+          modules={[Pagination, Navigation]}
+          breakpoints={{
+            0: {
+              slidesPerView: 2,
+              spaceBetween: 0
+            },
+            790: {
+              slidesPerView: 3,
+              spaceBetween: 20,
+            },
+            1020: {
+              slidesPerView: 4,
+              spaceBetween: 20,
+            },
+
+          }}
+        >
+          {products.map(product => (
+            <SwiperSlide key={product.id} >
+              <ItemList product={product} />
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </main>
     </main>
   )
 }
