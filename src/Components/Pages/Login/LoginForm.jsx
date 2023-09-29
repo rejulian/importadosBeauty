13873f4d69b2f1f877e75fb2ff0362cc7417b124
Re Julian <julianre2021@gmail.com>
2023-09-29 20:26:46 -0300

    Login user experience

--- src/Components/Pages/Login/LoginForm.jsx
+++ src/Components/Pages/Login/LoginForm.jsx
@@ -4,18 +4,21 @@ import { Button, FormControl, IconButton, InputAdornment, InputLabel, OutlinedIn
 import { Visibility, VisibilityOff } from "@mui/icons-material";
 import { Link, useNavigate } from 'react-router-dom';
 import { onSignIn, onSignInWithGoogle, db } from '../../../firebaseConfig';
-import {collection, doc, getDoc} from "firebase/firestore"
+import { collection, doc, getDoc } from "firebase/firestore"
 import './Auth.css'
 import { AuthContext } from '../../../Context/AuthContext';
+import { ClipLoader } from 'react-spinners';
 
 const LoginForm = () => {
     const navigate = useNavigate()
-    const {loginContext} = useContext(AuthContext)
+    const { loginContext } = useContext(AuthContext)
     const [showPassword, setShowPassword] = useState(false);
     const [userCredentials, setUserCredentials] = useState({
         email: "",
         password: ""
     });
+    const [error, setError] = useState(null);
+    const [isLoading, setIsLoading] = useState(false)
 
     const handleClickShowPassword = () => setShowPassword(!showPassword);
     const handleChange = (e) => {
@@ -26,7 +29,8 @@ const LoginForm = () => {
         e.preventDefault()
         try {
             const res = await onSignIn(userCredentials);
-            if(res.user){
+            setIsLoading(true)
+            if (res.user) {
                 const userCollection = collection(db, "users")
                 const userRef = doc(userCollection, res.user.uid)
                 const userDoc = await getDoc(userRef)
@@ -34,11 +38,15 @@ const LoginForm = () => {
                     email: res.user.email,
                     role: userDoc.data().role
                 }
+                setError(null)
+                setIsLoading(false)
                 loginContext(finalUser)
                 navigate('/')
             }
         } catch (error) {
             console.error(error)
+            setIsLoading(false)
+            setError('Error al iniciar sesion')
         }
     }
 
@@ -49,7 +57,7 @@ const LoginForm = () => {
                 email: res.user.email,
                 role: "user"
             }
-            if(res.user){
+            if (res.user) {
                 loginContext(finalUser)
                 navigate('/')
             }
@@ -59,44 +67,52 @@ const LoginForm = () => {
     }
 
     return (
-        <form onSubmit={handleLogin} className='auth-form'>
-            <h1>Iniciar sesion</h1>
-            <TextField onChange={handleChange} name='email' label='Email' fullWidth />
-            <FormControl variant='outlined'>
-                <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
-                <OutlinedInput
-                    onChange={handleChange}
-                    name='password'
-                    id='outlined-adornment-password'
-                    type={showPassword ? "text" : "password"}
-                    fullWidth
-                    endAdornment={
-                        <InputAdornment position='end'>
-                            <IconButton
-                                aria-label='toggle password visibility'
-                                edge="end"
-                                onClick={handleClickShowPassword}
-                            >
-                                {showPassword ? (
-                                    <VisibilityOff color='primary' />
-                                ) : (
-                                    <Visibility color='primary' />
-                                )}
-                            </IconButton>
-                        </InputAdornment>
-                    }
-                    label="Contraseña"
-                />
-            </FormControl>
-            <Button type='submit' variant='contained' fullWidth>Iniciar sesion</Button>
-            <Button onClick={handleLoginWithGoogle} className='btn-google' variant='outlined' fullWidth>
-                <img className='icon' src={Google} alt="" />
-                Ingresa con Google
-            </Button>
-            <Link className='links' to="/forgot-password">¿Olvidaste tu contraseña?</Link>
-            <Link className='links' to="/register">¿No tienes cuenta? Registrate</Link>
-
-        </form>
+        <>
+            {
+                isLoading ? (
+                    <ClipLoader color="#ffc1c1" loading={isLoading}/>
+                ) :
+                    <form onSubmit={handleLogin} className='auth-form'>
+                        <h1>Iniciar sesion</h1 >
+                        {error && <p className='error'><strong>{error}</strong></p>
+                        }
+                        <TextField onChange={handleChange} name='email' label='Email' fullWidth />
+                        <FormControl variant='outlined'>
+                            <InputLabel htmlFor="outlined-adornment-password">Contraseña</InputLabel>
+                            <OutlinedInput
+                                onChange={handleChange}
+                                name='password'
+                                id='outlined-adornment-password'
+                                type={showPassword ? "text" : "password"}
+                                fullWidth
+                                endAdornment={
+                                    <InputAdornment position='end'>
+                                        <IconButton
+                                            aria-label='toggle password visibility'
+                                            edge="end"
+                                            onClick={handleClickShowPassword}
+                                        >
+                                            {showPassword ? (
+                                                <VisibilityOff color='primary' />
+                                            ) : (
+                                                <Visibility color='primary' />
+                                            )}
+                                        </IconButton>
+                                    </InputAdornment>
+                                }
+                                label="Contraseña"
+                            />
+                        </FormControl>
+                        <Button type='submit' variant='contained' fullWidth>Iniciar sesion</Button>
+                        <Button onClick={handleLoginWithGoogle} className='btn-google' variant='outlined' fullWidth>
+                            <img className='icon' src={Google} alt="" />
+                            Ingresa con Google
+                        </Button>
+                        <Link className='links' to="/forgot-password">¿Olvidaste tu contraseña?</Link>
+                        <Link className='links' to="/register">¿No tienes cuenta? Registrate</Link>
+                    </form >
+            }
+        </>
     )
 }
 
